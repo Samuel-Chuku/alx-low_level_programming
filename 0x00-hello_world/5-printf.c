07e47ddb3cfb969ac0ddf232ea9801e87560ea46
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-10 19:56:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code.
+ * Return: This will return 0 if the code runs successfully.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
