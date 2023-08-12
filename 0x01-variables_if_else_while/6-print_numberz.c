7b183282fd96954f2398fba5c7b47111d2c79535
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-12 16:29:43 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code.
+ *Description: This will print digits.
+ *Return: This will return 0 upon successful execution of the code.
+ */
+int main(void)
+{
+	int s = 48;
+
+	while (s <= 57)
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
