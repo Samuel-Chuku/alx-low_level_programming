10c7815038a9c5dad952c84a6b522c4c6d937166
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-12 03:47:33 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Main entry point of code.
+ * Return: returns 1 for this code.
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
