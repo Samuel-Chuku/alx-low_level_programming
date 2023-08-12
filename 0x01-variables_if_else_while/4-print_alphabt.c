26d43613e5b25db3eafbf445253ddc4527753573
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-12 16:09:19 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This is the main entry of the code
+ * Description: This program will print all alphabets in lowercase.
+ * Return: Will return 0 when the code is run successfully.
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
