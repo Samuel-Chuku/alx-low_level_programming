ff97a7a22d593f18bec6a2eac2b975238c25022a
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-15 16:48:51 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This is the main entry of the code.
+ * Description: This is a program that prints the string _putchar to the terminal.
+ * Return: The program will return 0 after successful execution.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme for the 0x02-functions_nested_loops repo
