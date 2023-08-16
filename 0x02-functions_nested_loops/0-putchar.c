196d80c6c33832e22d5e27f98c95a9a2f3f768e1
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-15 18:08:41 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
  * main - This is the main entry of the code.
  * Description: This is a program that prints the string _putchar to the terminal.
@@ -7,7 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
