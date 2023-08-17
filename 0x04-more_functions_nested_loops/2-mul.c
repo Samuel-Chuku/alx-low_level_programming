2624e1b857ff7b0dbe1b0fc3b55f3015e8eb7a74
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-17 16:51:21 -0700

    Program that prints the multiplication of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - This program will print the
+ * multiplication of 2 integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Will return the result of the
+ * multiplication.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
