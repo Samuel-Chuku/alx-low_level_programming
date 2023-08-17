1ac44100eb8610a679f1e1cfb1d9b8095c1b7861
Samuel Chuku <samarasuzi3@gmail.com>
2023-08-16 18:44:09 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Program that adds two integers and
+ * prints the result.
+ * @s: First number to be added
+ * @c: Second number to be added
+ * Return: result of addition
+ */
+
+int add(int s, int c)
+{
+	return (s + c);
+}
